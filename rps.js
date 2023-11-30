869923dc3cc767a7cf2855a851697f072f487f85
adrianceriotti <adrianearle16@yahoo.com>
2023-11-30 02:13:08 -0800

    Made single round of game that displays a winner wrote if statements for all potential outcomes

--- rps.js
+++ rps.js
@@ -1,10 +1,43 @@
-// Get first computers choice
-let gameOptions = ["Rock", "Paper", "Scissors"]
+// Get computers choice
+let gameOptions = ["rock", "paper", "scissors"]
 function getComputerChoice(){
 return (gameOptions[(Math.floor(Math.random() * gameOptions.length))]);
 }
-let compChoice = getComputerChoice();
-console.log(compChoice)
+//let computerChoice = getComputerChoice();
+//console.log(computerChoice)
 // Prompt User for rock paper scissor 
-//allow user to type non case sensitive 
+//let playerChoice = prompt("Rock, Paper, Or Scissors").toLowerCase();
 //compare if 1st is greater than 2nd
+function playRound(computerChoice, playerChoice){
+if (playerChoice === computerChoice){
+alert("This is a tie")
+}
+else if (playerChoice === "rock") {
+    if(computerChoice=== "paper"){
+    alert("Computer Won")
+}
+else {
+    alert("Player Won")
+}
+}
+else if (playerChoice === "paper"){
+if (computerChoice === "scissors"){
+alert("Computer Won")
+}
+else{
+    alert("Player Won")
+}
+}
+else if(playerChoice === "scissors"){
+    if(computerChoice === "rock"){
+        alert("Computer Won")
+    }
+    else{
+        alert("Player Won")
+    }
+}
+}
+
+const playerChoice = "rock";
+const computerChoice= getComputerChoice();
+console.log(playRound(computerChoice, playerChoice));
