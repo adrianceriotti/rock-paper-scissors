7d6b0eedd801e98aef884e2ada07e1d629e4964b
adrianceriotti <adrianearle16@yahoo.com>
2023-12-01 13:41:14 -0800

    results now get logged to console increment fixed so that total score adds to 5

--- rps.js
+++ rps.js
@@ -1,43 +1,71 @@
-// Get computers choice
+let playerScore = 0
+let computerScore = 0
+let draws = 0
+const playerChoice = "rock";
 let gameOptions = ["rock", "paper", "scissors"]
+// Get computers choice
 function getComputerChoice(){
 return (gameOptions[(Math.floor(Math.random() * gameOptions.length))]);
 }
-//let computerChoice = getComputerChoice();
-//console.log(computerChoice)
 // Prompt User for rock paper scissor 
 //let playerChoice = prompt("Rock, Paper, Or Scissors").toLowerCase();
-//compare if 1st is greater than 2nd
+
+//function to determine winner and keep score
 function playRound(computerChoice, playerChoice){
 if (playerChoice === computerChoice){
-alert("This is a tie")
+    draws++
+    return("This is a draw")
 }
 else if (playerChoice === "rock") {
     if(computerChoice=== "paper"){
-    alert("Computer Won")
-}
-else {
-    alert("Player Won")
-}
+    computerScore++
+    return("You Lose Paper beats Rock")  
+}else{
+    playerScore++
+    return("You Win Rock beats Scissors")  
+    }
 }
 else if (playerChoice === "paper"){
-if (computerChoice === "scissors"){
-alert("Computer Won")
-}
-else{
-    alert("Player Won")
-}
+    if (computerChoice === "scissors"){
+    computerScore++
+    return("You lose Scissors beats Paper")
+}else{
+    playerScore++
+    return("You win Paper beats Rock")
+    }
 }
 else if(playerChoice === "scissors"){
     if(computerChoice === "rock"){
-        alert("Computer Won")
-    }
-    else{
-        alert("Player Won")
+        computerScore++
+        return("You lose Rock beats Scissors")
+}else{
+        playerScore++
+    return("You win Scissors beats Paper")
     }
 }
 }
 
-const playerChoice = "rock";
-const computerChoice= getComputerChoice();
+//function to play multiple rounds
+function game(){
+for (var i= 0; i<5; i++){
+let computerChoice = getComputerChoice();
 console.log(playRound(computerChoice, playerChoice));
+}
+if (playerScore > computerScore){
+    return ("You Win");
+} else{
+    return ("GG Loser");
+}
+
+}
+
+game()
+console.log(draws)
+console.log(playerScore)
+console.log(computerScore)
+
+
+
+
+
+
